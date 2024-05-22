26eb81d38e00cccb29eee8b5c43903e733453e2b
Terry McGuinness <terry.mcguinness@noaa.gov>
2024-05-22 18:24:03 -0500

    excludes updating config dic with values that are in enviroment and in config files when parcing EXPDIR config files

--- src/wxflow/configuration.py
+++ src/wxflow/configuration.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import random
+from typing import Optional
 import subprocess
 from pathlib import Path
 from pprint import pprint
@@ -79,7 +80,8 @@ class Configuration:
         if isinstance(files, (str, bytes)):
             files = [files]
         files = [self.find_config(file) for file in files]
-        return cast_strdict_as_dtypedict(self._get_script_env(files))
+        config_dict = self._get_script_env(files)  # get the config dictionary from the config files
+        return cast_strdict_as_dtypedict(self._get_script_env(files, config_dict))
 
     def print_config(self, files: Union[str, bytes, list]) -> None:
         """
@@ -93,9 +95,11 @@ class Configuration:
         pprint(config, width=4)
 
     @classmethod
-    def _get_script_env(cls, scripts: List) -> Dict[str, Any]:
+    def _get_script_env(cls, scripts: List, config_dict: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
         default_env = cls._get_shell_env([])
         and_script_env = cls._get_shell_env(scripts)
+        if config_dict is not None:  # only update the environment with the config dictionary if it is provided
+            default_env = {k: v for k, v in default_env.items() if k in config_dict}
         vars_just_in_script = set(and_script_env) - set(default_env)
         union_env = dict(default_env)
         union_env.update(and_script_env)
